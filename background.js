a7055dc720c40aa5a9aa7cbb9472f91019dbfa25
William Pieh <wtpieh@gmail.com>
2024-08-06 05:37:14 -0700

    Fixed extension

--- background.js
+++ background.js
@@ -13,9 +13,10 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+const __DOMAINNAME__ = "https://webapi-elzoglb7jq-uc.a.run.app";
 const __HOSTNAME__ = "https://halopl.us";
 const __LOCAL_DB__ = chrome.storage.sync;
-const __API__ = `${__HOSTNAME__}/api`;
+const __API__ = `${__DOMAINNAME__}/api`;
 const __VERSION__ = chrome.runtime.getManifest().version;
 
 const isCookieValid = (obj) => {
@@ -26,23 +27,38 @@ const isCookieValid = (obj) => {
 
 const getCookies = async (url = "https://halo.gcu.edu") => {
   try {
-    return (await chrome.cookies.getAll({ url })).reduce(
-      (acc, { name, value }) => ({ ...acc, [name]: value }),
-      {}
-    );
+    console.log("Getting cookies");
+    const cookies = await chrome.cookies.getAll({ url });
+    console.log("Cookies fetched:", cookies);
+
+    // Ensure cookies is an array before calling reduce
+    if (Array.isArray(cookies)) {
+      const cookieData = cookies.reduce((acc, { name, value }) => {
+        acc[name] = value;
+        return acc;
+      }, {});
+      console.log("Reduced cookie data:", cookieData);
+      return cookieData;
+    } else {
+      console.error("Expected an array of cookies, but got:", cookies);
+      return {};
+    }
   } catch (e) {
-    console.log(e);
+    console.error("Error getting cookies:", e);
     return {};
   }
 };
 
 const pushCookiesToDatabase = async ({ cookie, auth_session }) => {
   // Send data to API so we can automate checking intervals.
+  console.log("auth: " + cookie["TE1TX0FVVEg"]);
+  console.log("context: " + cookie["TE1TX0NPTlRFWFQ"]);
+  console.log("csrf: " + cookie["__Host-next-auth.csrf-token"]);
   return await fetch(
-    `${__API__}/users/${auth_session}/update?auth_token=${cookie["TE1TX0FVVEg"]}&context_token=${cookie["TE1TX0NPTlRFWFQ"]}&csrf_token=${cookie["csrf"]}`,
+    `${__API__}/users/${auth_session}/update?auth_token=${cookie["TE1TX0FVVEg"]}&context_token=${cookie["TE1TX0NPTlRFWFQ"]}&csrf_token=${cookie["__Host-next-auth.csrf-token"]}`,
     {
       method: "POST",
-    }
+    },
   )
     .then(() => true)
     .catch((err) => {
@@ -52,32 +68,42 @@ const pushCookiesToDatabase = async ({ cookie, auth_session }) => {
 };
 
 async function syncCookiesToDatabase() {
-  const { TE1TX0FVVEg, TE1TX0NPTlRFWFQ, csrf } = await __LOCAL_DB__.get([
-    "TE1TX0FVVEg",
-    "TE1TX0NPTlRFWFQ",
-    "csrf"
-  ]);
+  try {
+    const { TE1TX0FVVEg, TE1TX0NPTlRFWFQ, csrf } = await __LOCAL_DB__.get([
+      "TE1TX0FVVEg",
+      "TE1TX0NPTlRFWFQ",
+      "__Host-next-auth.csrf-token",
+    ]);
+    console.log("TE1TX0FVVEg: " + TE1TX0FVVEg);
+    console.log("TE1TX0NPTlRFWFQ: " + TE1TX0NPTlRFWFQ);
+    console.log("csrf: " + csrf);
 
-  var { auth_session } = await getCookies(__HOSTNAME__);
+    const cookies = await getCookies(__HOSTNAME__);
+    const auth_session = cookies["auth_session"];
 
-  if (!!TE1TX0FVVEg && !!TE1TX0NPTlRFWFQ && !!csrf) {
-    const success = await pushCookiesToDatabase({
-      cookie: {
-        TE1TX0FVVEg,
-        TE1TX0NPTlRFWFQ,
-        csrf
-      },
-      auth_session,
-    });
-    console.log(
-      success
-        ? "[Halo+] Updated cookie to database."
-        : "[Halo+] Failed to update cookie to database."
-    );
-    return success;
+    if (TE1TX0FVVEg && TE1TX0NPTlRFWFQ && csrf) {
+      const success = await pushCookiesToDatabase({
+        cookie: {
+          TE1TX0FVVEg,
+          TE1TX0NPTlRFWFQ,
+          csrf,
+        },
+        auth_session,
+      });
+
+      console.log(
+        success
+          ? "[Halo+] Updated cookie to database."
+          : "[Halo+] Failed to update cookie to database.",
+      );
+      return success;
+    }
+
+    return false;
+  } catch (error) {
+    console.error("Error syncing cookies to database:", error);
+    return false;
   }
-
-  return false;
 }
 
 (async () => {
@@ -85,11 +111,16 @@ async function syncCookiesToDatabase() {
 
   chrome.runtime.onConnect.addListener(async (port) => {
     if (port.name === "auth_session") {
-      var { auth_session } = await getCookies(__HOSTNAME__);
-      if (auth_session == null) {
+      console.log("[Halo+] Received auth_session request");
+      const cookies = await getCookies(__HOSTNAME__);
+      const auth_session = cookies["auth_session"];
+      console.log("auth_session: " + auth_session);
+
+      if (!auth_session) {
         console.log("[Halo+] User doesn't have cookie");
         return port.postMessage({ auth_session: null });
       }
+      console.log("[Halo+] User has cookie");
 
       const isAuthenticated = await fetch(
         `${__API__}/users/${auth_session}/validate`,
@@ -99,7 +130,7 @@ async function syncCookiesToDatabase() {
             accept: "*/*",
             "content-type": "application/json",
           },
-        }
+        },
       )
         .then((res) => res.status === 200)
         .catch(() => false);
@@ -113,33 +144,31 @@ async function syncCookiesToDatabase() {
     } else if (port.name === "send_cookies") {
       const cookies = await getCookies();
       const { TE1TX0FVVEg, TE1TX0NPTlRFWFQ } = cookies;
-      const csrf = cookies['__Host-next-auth.csrf-token'];
+      const csrf = cookies["__Host-next-auth.csrf-token"];
+      console.log("TE1TX0FVVEg: " + TE1TX0FVVEg);
+      console.log("TE1TX0NPTlRFWFQ: " + TE1TX0NPTlRFWFQ);
+      console.log("csrf: " + csrf);
 
       const { last_updated } = await __LOCAL_DB__.get("last_updated");
-      if (last_updated && (Date.now() - last_updated) < 3600000) {
+      if (last_updated && Date.now() - last_updated < 3600000) {
         console.log("[Halo+] User is on cooldown!");
         return port.postMessage({ success: false });
       }
 
       await __LOCAL_DB__.set({ last_updated: Date.now() });
 
-      const { TE1TX0FVVEg: stored_auth_token } = await __LOCAL_DB__.get(
-        "TE1TX0FVVEg"
-      );
-      if (stored_auth_token != TE1TX0FVVEg)
+      const { TE1TX0FVVEg: stored_auth_token } =
+        await __LOCAL_DB__.get("TE1TX0FVVEg");
+      if (stored_auth_token !== TE1TX0FVVEg)
         await __LOCAL_DB__.set({ TE1TX0FVVEg });
 
-      const { TE1TX0NPTlRFWFQ: stored_context_token } = await __LOCAL_DB__.get(
-        "TE1TX0NPTlRFWFQ"
-      );
-      if (stored_context_token != TE1TX0NPTlRFWFQ)
+      const { TE1TX0NPTlRFWFQ: stored_context_token } =
+        await __LOCAL_DB__.get("TE1TX0NPTlRFWFQ");
+      if (stored_context_token !== TE1TX0NPTlRFWFQ)
         await __LOCAL_DB__.set({ TE1TX0NPTlRFWFQ });
 
-      const { csrf: stored_csrf_token } = await __LOCAL_DB__.get(
-        "csrf"
-      );
-      if (stored_csrf_token != csrf)
-        await __LOCAL_DB__.set({ csrf });
+      const { csrf: stored_csrf_token } = await __LOCAL_DB__.get("csrf");
+      if (stored_csrf_token !== csrf) await __LOCAL_DB__.set({ csrf });
 
       return port.postMessage({ success: await syncCookiesToDatabase() });
     }
@@ -148,12 +177,12 @@ async function syncCookiesToDatabase() {
   chrome.cookies.onChanged.addListener(async ({ cookie }) => {
     if (!cookie || !Object.keys(cookie).length)
       return console.log(
-        "[Halo+] Cookie object was determined to be invalid. Skipping process."
+        "[Halo+] Cookie object was determined to be invalid. Skipping process.",
       );
 
     if (cookie.domain === "halo.gcu.edu") {
       const stored_cookie = await __LOCAL_DB__.get(cookie.name);
-      if (stored_cookie[cookie.name] != cookie.value) {
+      if (stored_cookie[cookie.name] !== cookie.value) {
         // Check if the value actually updated.
         await __LOCAL_DB__.set({ [cookie.name]: cookie.value });
         await syncCookiesToDatabase();
